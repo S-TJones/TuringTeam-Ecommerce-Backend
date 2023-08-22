d1ff5d2d91b77fca852e2bf242eadff3d379e289
S-TJones <jones.shemar.sj@gmail.com>
2023-08-22 18:44:08 -0500

    Added Login and SignUp

--- myAppFrontend/src/pages/Login.tsx
+++ myAppFrontend/src/pages/Login.tsx
@@ -1,9 +1,45 @@
 import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
 import ExploreContainer from '../components/ExploreContainer';
 
+import React, { useState } from 'react';
+
 // The replaceable components
 
 const Login: React.FC = () => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleLogin = () => {
+    // Create a JSON object with the user's credentials
+    const credentials = {
+      email,
+      password,
+    };
+
+    // Send a POST request to your Flask authentication route
+    fetch('/login', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(credentials),
+    })
+    .then((response) => {
+      if (response.ok) {
+        // Authentication succeeded
+        // Redirect the user to the protected part of your app
+        window.location.href = '/dashboard'; // Example redirection
+      } else {
+        // Authentication failed
+        // Handle error or show an error message to the user
+      }
+    })
+    .catch((error) => {
+      console.error('Error during login:', error);
+      // Handle network errors or other issues
+    });
+  };
+
   return (
     <IonPage>
       {/* Header */}
@@ -24,9 +60,20 @@ const Login: React.FC = () => {
         {/* Main Content - Switchable Components */}
         <main>
           <div>
-            <p>Some static content goes here...</p>
-            
-            {/* Add more content as needed */}
+            <h1>Login</h1>
+            <input
+              type="email"
+              placeholder="Email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+            />
+            <input
+              type="password"
+              placeholder="Password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+            />
+            <button onClick={handleLogin}>Login</button>
           </div>
         </main>
 
--- myAppFrontend/src/pages/SignUp.tsx
+++ myAppFrontend/src/pages/SignUp.tsx
@@ -1,9 +1,52 @@
 import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
 import ExploreContainer from '../components/ExploreContainer';
 
+import React, { useState } from 'react';
+
 // The replaceable components
 
 const SignUp: React.FC = () => {
+
+  const [firstName, setFirstName] = useState('');
+  const [lastName, setLastName] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [confirmPassword, setConfirmPassword] = useState('');
+
+  const handleSignup = () => {
+    // Create a JSON object with the user's signup data
+    const userData = {
+      firstName,
+      lastName,
+      email,
+      password,
+      confirmPassword,
+    };
+
+    // Send a POST request to your Flask signup route
+    fetch('/signup', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(userData),
+    })
+      .then((response) => {
+        if (response.ok) {
+          // Signup succeeded
+          // Redirect the user to the login page or a success page
+          window.location.href = '/login'; // Example redirection
+        } else {
+          // Signup failed
+          // Handle error or show an error message to the user
+        }
+      })
+      .catch((error) => {
+        console.error('Error during signup:', error);
+        // Handle network errors or other issues
+      });
+  };
+
   return (
     <IonPage>
       {/* Header */}
@@ -24,9 +67,38 @@ const SignUp: React.FC = () => {
         {/* Main Content - Switchable Components */}
         <main>
           <div>
-            <p>Some static content goes here...</p>
-            
-            {/* Add more content as needed */}
+            <h1>Signup</h1>
+            <input
+              type="text"
+              placeholder="First Name"
+              value={firstName}
+              onChange={(e) => setFirstName(e.target.value)}
+            />
+            <input
+              type="text"
+              placeholder="Last Name"
+              value={lastName}
+              onChange={(e) => setLastName(e.target.value)}
+            />
+            <input
+              type="email"
+              placeholder="Email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+            />
+            <input
+              type="password"
+              placeholder="Password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+            />
+            <input
+              type="password"
+              placeholder="Confirm Password"
+              value={confirmPassword}
+              onChange={(e) => setConfirmPassword(e.target.value)}
+            />
+            <button onClick={handleSignup}>Signup</button>
           </div>
         </main>
 
