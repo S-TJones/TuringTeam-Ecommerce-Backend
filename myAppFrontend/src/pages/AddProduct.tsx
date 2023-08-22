824f99e669b1880fff86ab6b8e745c6c1135356a
S-TJones <jones.shemar.sj@gmail.com>
2023-08-22 14:34:50 -0500

    Update pages

---
+++ myAppFrontend/src/pages/AddProduct.tsx
@@ -0,0 +1,43 @@
+import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
+import ExploreContainer from '../components/ExploreContainer';
+
+// The replaceable components
+
+const AddProduct: React.FC = () => {
+  return (
+    <IonPage>
+      {/* Header */}
+      <IonHeader>
+        <IonToolbar>
+          <IonTitle>Blank</IonTitle>
+        </IonToolbar>
+      </IonHeader>
+
+      {/* Content Area */}
+      <IonContent fullscreen>
+        <IonHeader collapse="condense">
+          <IonToolbar>
+            <IonTitle size="large">Blank</IonTitle>
+          </IonToolbar>
+        </IonHeader>
+
+        {/* Main Content - Switchable Components */}
+        <main>
+          <div>
+            <p>Some static content goes here...</p>
+            
+            {/* Add more content as needed */}
+          </div>
+        </main>
+
+        {/* Footer */}
+        <footer>
+          <p>TuringTeam &copy</p>
+        </footer>
+        <ExploreContainer />
+      </IonContent>
+    </IonPage>
+  );
+};
+
+export default AddProduct;
