d82f62324bcbda72fa97ffd30d3bbb69fd0dcbb4
rushionfisher <94405505+rushionfisher@users.noreply.github.com>
2023-08-22 23:26:47 -0500

    Update Cart.tsx

--- myAppFrontend/src/pages/Cart.tsx
+++ myAppFrontend/src/pages/Cart.tsx
@@ -1,7 +1,17 @@
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
+import React from 'react';
+import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar, IonButton, IonButtons, IonSelectOption,  IonItem, IonLabel, IonSelect, IonGrid, IonRow, IonCol,  } from '@ionic/react';
 import ExploreContainer from '../components/ExploreContainer';
+import { Link } from 'react-router-dom';
+import logoImage from '../images/img1.jpeg'; // Import your logo image
+import './Cart.css';
+
 
 // The replaceable components
+const items = [
+  { id: 1, name: 'Chair', price: 20.00, image: 'src/images/img1.jpeg', quantity: 1 },
+  { id: 2, name: 'Backpack', price: 25.00, image: 'src/images/img1.jpeg', quantity: 2 },
+  // Add more products as needed
+];
 
 const Cart: React.FC = () => {
   return (
@@ -9,35 +19,76 @@ const Cart: React.FC = () => {
       {/* Header */}
       <IonHeader>
         <IonToolbar>
-          <IonTitle>Blank</IonTitle>
+          {/* Add image to the far left */}
+          <div slot="start">
+            <img src={logoImage} alt="Logo" style={{ height: '30px', width: 'auto' }} />
+          </div>
+          
+          {/* Title in the center */}
+          <IonTitle className="ion-text-left ion-text-bold title-left">Turing's Shop</IonTitle>
+          <IonButtons slot="end">
+            <Link to="/" className='tabs'>
+              <IonButton>
+                Home
+              </IonButton>
+            </Link>
+            <Link to="/product" className='tabs'>
+              <IonButton>
+                Products
+              </IonButton>
+            </Link>
+            <Link to="/orders" className='tabs'>
+              <IonButton>
+                Orders
+              </IonButton>
+            </Link>
+            <Link to="/cart" className='tabs'>
+              <IonButton>
+                Cart
+              </IonButton>
+            </Link>
+            <Link to="/login" className='tabs'>
+              <IonButton>
+                Login
+              </IonButton>
+            </Link>
+          </IonButtons>
         </IonToolbar>
       </IonHeader>
 
-      {/* Content Area */}
-      <IonContent fullscreen>
-        <IonHeader collapse="condense">
-          <IonToolbar>
-            <IonTitle size="large">Blank</IonTitle>
-          </IonToolbar>
-        </IonHeader>
-
-        {/* Main Content - Switchable Components */}
+    <IonContent fullscreen>
         <main>
-          <div>
-            <p>Some static content goes here...</p>
-            
-            {/* Add more content as needed */}
-          </div>
+          <h2 className="user-list-header">Cart</h2>
+          {items.map((item) => (
+            <div key={item.id} className="cart-item">
+              <div className="product-image">
+                <img src={item.image} alt="Product" />
+              </div>
+              <div className="product-details">
+                <h3>{item.name}</h3>
+                <div className="product-price">${item.price.toFixed(2)}</div>
+                <div className="quantity">
+                  <label>Quantity</label>
+                  <select>
+                    <option value="1">1</option>
+                    <option value="2">2</option>
+                    <option value="3">3</option>
+                    {/* Add more options as needed */}
+                  </select>
+                </div>
+              </div>
+            </div>
+          ))}
+          <IonButton className="chekout-button" fill="clear">Checkout</IonButton>
         </main>
-
         {/* Footer */}
         <footer>
-          <p>TuringTeam &copy</p>
+          <p>The Turing Team &copy;</p>
         </footer>
-        <ExploreContainer />
+      
       </IonContent>
     </IonPage>
   );
-};
+}
 
 export default Cart;
